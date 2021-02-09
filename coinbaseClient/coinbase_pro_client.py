b70885a5f4f26e1a5e7e7d9ec4fd874e2ea3bef0
joshcline002 <joshcline002@gmail.com>
2021-02-08 18:11:38 -0600

    Upload initial stuff

---
+++ coinbaseClient/coinbase_pro_client.py
@@ -0,0 +1,55 @@
+import cbpro
+import requests
+
+from Config.config import CB_CREDENTIALS
+
+def get_private_client(credentials):
+    """Returns the cbpro AuthenticatedClient using the credentials from the parameters dict"""
+    private_client = cbpro.AuthenticatedClient(credentials['KEY'], credentials['SECRET'], credentials['PASSPHRASE'], api_url=credentials['URL'])
+    return private_client
+
+def cbpro_private_client(func):
+    def function_wrapper(*args, **kwargs):
+        private_client = get_private_client(CB_CREDENTIALS)
+        resp = func(private_client = private_client, *args, **kwargs)
+        return resp
+    return function_wrapper
+
+def get_public_client(url):
+    public_client = cbpro.PublicClient(api_url=url)
+    return public_client
+
+def cbpro_public_client(func):
+    def function_wrapper(*args, **kwargs):
+        public_client = get_public_client(url=CB_CREDENTIALS['URL'])
+        resp = func(public_client = public_client, *args, **kwargs)
+        return resp
+    return function_wrapper
+
+@cbpro_private_client
+def get_prices(private_client):
+    resp = private_client._send_message('get', '/oracle')
+    prices = resp['prices']
+    return prices
+
+@cbpro_private_client
+def get_product_ticker(private_client, currency):
+    ticker = private_client.get_product_ticker(f'{currency}-USD')
+    if ticker == 'NotFound':
+        ticker = None
+    return ticker
+
+@cbpro_public_client
+def get_products(public_client):
+    products = public_client.get_products()
+    return products
+
+@cbpro_private_client
+def get_accounts(private_client):
+    accounts = private_client.get_accounts()
+    return accounts
+
+@cbpro_private_client
+def place_order(private_client, product_id, side, funds):
+    place_order = private_client.place_order(product_id=product_id, side=side, order_type='market', funds=funds)
+    return place_order
---
+++ run_2mins.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true
+do
+  clear
+  python3 main.py
+  sleep 30
+done
