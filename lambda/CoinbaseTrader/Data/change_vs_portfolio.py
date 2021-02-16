4662b3cb6f184f73081e5c20d8ae79a3294e36c9
joshcline002 <joshcline002@gmail.com>
2021-02-15 20:47:59 -0600

    Working my pipeline stuff

---
+++ lambda/CoinbaseTrader/Data/change_vs_portfolio.py
@@ -0,0 +1,20 @@
+
+
+def set_and_sort_crypto_change_vs_portfolio(cryptos, portfolio_value, quote_currency):
+  max_change_vs_portfolio = 0
+  quote_currency_amount = 0
+  for crypto in cryptos:
+      current_percent = (crypto["MarketValue"]/portfolio_value)*100
+      crypto['CurrentPercent'] = current_percent
+      if crypto['TargetPercent'] != 0:
+          change_vs_portfolio = (current_percent/crypto['TargetPercent']*100) - 100
+          crypto['ChangeVsPortfolio'] = change_vs_portfolio
+          absolute_change = abs(change_vs_portfolio)
+          if absolute_change > max_change_vs_portfolio and crypto['Crypto'] != quote_currency:
+              max_change_vs_portfolio = absolute_change
+          else:
+              crypto['ChangeVsPortfolio'] = 0
+      if crypto['Crypto'] == quote_currency:
+          quote_currency_amount = crypto['MarketValue']
+  cryptos = sorted(cryptos, key = lambda i: i['ChangeVsPortfolio'], reverse=True)
+  return cryptos, max_change_vs_portfolio, quote_currency_amount
---
+++ lambda/CoinbaseTrader/Data/product.py
@@ -0,0 +1,27 @@
+from coinbaseClient.coinbase_pro_client import get_products
+
+
+def get_products_for_quote_currency_info(quote_currency):
+    print(get_products.return_value)
+    products = get_products()
+    products_to_quote_currency_info = {}
+    print(products)
+    for product in products:
+        if product['quote_currency'] != quote_currency:
+            continue
+        products_to_quote_currency_info[product['base_currency']] ={
+                'base_min_size': float(product['base_min_size']),
+                'base_max_size': float(product['base_max_size']),
+                'min_market_funds': float(product['min_market_funds']),
+                'max_market_funds': float(product['max_market_funds']),
+                f'{quote_currency}_round_to_digit': get_precision_of_decimal(product['quote_increment']),
+                'shares_round_to_digit': get_precision_of_decimal(product['base_increment'])
+            }
+    return products_to_quote_currency_info
+
+def get_precision_of_decimal(decimal_num):
+    split_on_zero = decimal_num.split('.')
+    get_numbers_after_decimal = split_on_zero[-1]
+    removed_trailing_zeros = get_numbers_after_decimal.rstrip('0')
+    precision = len(removed_trailing_zeros)
+    return precision
