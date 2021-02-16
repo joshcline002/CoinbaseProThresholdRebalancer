caff46c885d7e4e73694e0c98912b65b75121497
joshcline002 <joshcline002@gmail.com>
2021-02-15 20:21:52 -0600

    Sup boyyyyy

--- CoinbaseTrader/Data/product.py
+++ CoinbaseTrader/Data/product.py
@@ -2,9 +2,10 @@ from coinbaseClient.coinbase_pro_client import get_products
 
 
 def get_products_for_quote_currency_info(quote_currency):
+    print(get_products.return_value)
     products = get_products()
     products_to_quote_currency_info = {}
-
+    print(products)
     for product in products:
         if product['quote_currency'] != quote_currency:
             continue
@@ -13,7 +14,14 @@ def get_products_for_quote_currency_info(quote_currency):
                 'base_max_size': float(product['base_max_size']),
                 'min_market_funds': float(product['min_market_funds']),
                 'max_market_funds': float(product['max_market_funds']),
-                f'{quote_currency}_round_to_digit': len(product['quote_increment'].split('.')[-1].rstrip('0')),
-                'shares_round_to_digit': len(product['base_increment'].split('.')[-1].rstrip('0'))
+                f'{quote_currency}_round_to_digit': get_precision_of_decimal(product['quote_increment']),
+                'shares_round_to_digit': get_precision_of_decimal(product['base_increment'])
             }
     return products_to_quote_currency_info
+
+def get_precision_of_decimal(decimal_num):
+    split_on_zero = decimal_num.split('.')
+    get_numbers_after_decimal = split_on_zero[-1]
+    removed_trailing_zeros = get_numbers_after_decimal.rstrip('0')
+    precision = len(removed_trailing_zeros)
+    return precision
