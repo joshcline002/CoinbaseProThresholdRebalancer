f216e7e660b8f6066c7d01201e9dda098f0fde8d
joshcline002 <joshcline002@gmail.com>
2021-02-15 20:47:45 -0600

    Working my pipeline stuff

--- deploy_linux.py
+++ deploy_linux.py
@@ -1,21 +1,9 @@
 import os
-import shutil
 import subprocess
 import sys
 
 path = os.path.dirname(os.path.realpath(__file__))
-lambda_dir = 'CoinbaseTrader'
-print("Before copying file:")
-print(os.listdir(path))
-
-src = f'{path}/{lambda_dir}'
-dest = f'{path}/{lambda_dir}_deploy'
-destination = shutil.copytree(src, dest)
-print("After copying file:")
-print(os.listdir(path))
-
-print("Destination path:", destination)
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f'{destination}/requirement.txt', f'--target={destination}'])
-
-shutil.rmtree(dest)
+lambdas = f"{path}/{lambda_dir}"
+lambda_dirs = os.listdir(lambdas))
+for dir in lambda_dirs:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f'{destination}/requirement.txt', f'--target={destination}'])
