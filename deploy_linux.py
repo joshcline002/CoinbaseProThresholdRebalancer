ab2b841e89cf4d9387e16cc600908162b7cdb1f5
joshcline002 <joshcline002@gmail.com>
2021-02-15 21:18:27 -0600

    fixed bug in deploy python

--- deploy_linux.py
+++ deploy_linux.py
@@ -2,8 +2,15 @@ import os
 import subprocess
 import sys
 
-path = os.path.dirname(os.path.realpath(__file__))
-lambdas = f"{path}/{lambda_dir}"
-lambda_dirs = os.listdir(lambdas))
-for dir in lambda_dirs:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f'{destination}/requirement.txt', f'--target={destination}'])
+
+def main():
+    path = os.path.dirname(os.path.realpath(__file__))
+    lambdas = f"{path}/lambda"
+    lambda_dirs = os.listdir(lambdas)
+    for dir in lambda_dirs:
+        destination = f'{lambdas}/{dir}'
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", f'{destination}/requirement.txt', f'--target={destination}'])
+    print("complete")
+
+
+main()
