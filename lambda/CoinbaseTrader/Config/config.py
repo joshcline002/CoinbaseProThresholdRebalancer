b374d0bf0fb6b7489c0fd04a430915062d269e31
joshcline002 <joshcline002@gmail.com>
2021-02-23 21:18:00 -0600

    update to latest code fixes

---
+++ lambda/CoinbaseTrader/Config/config.py
@@ -0,0 +1,21 @@
+CB_CREDENTIALS = {
+'PASSPHRASE': '6wm4ld9si53', # 'pspdlxchhw',
+'SECRET': 'QjEbmbN8W9S/8gkTd8FR+fXFlyo4p1T4+CWrE4yt6CHyQLfW6LoBfzitj/lYEiAVxOsrSkJDMoNNlqGAKLYvwA==', # 'aHvakL9kJ8RbmIJU06BVAnS0/oLwgF+hmWiz6a71PgCxvDN6v4DHDe//6PpiB6AhM9FHXHvCL6xhidaMlZKW0Q==',
+'KEY': 'b585f4187010e8cea63648e591a665e3', # '840c5b0d3857414f7495172aca418095',
+'URL': "https://api-public.sandbox.pro.coinbase.com" # "https://api.pro.coinbase.com"
+}
+
+ENABLE_TRADING = False
+
+TARGET_PERCENTS = {
+'USD': 50,
+'BTC': 50
+}
+
+REBALANCE_TARGET = 15
+REBALANCE_TOLERANCE = 5
+IGNORE_MARKET_VALUE_LESS_THAN = 1
+
+FEE_PERCENT = .5
+
+CRYPTO_EXCLUDE_FROM_TRADING = ['XRP']
